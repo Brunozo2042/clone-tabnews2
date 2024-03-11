c7871f460af84511c371357f59c23264f60f3954
Bruno Henrique <bruno_henriquemo@hotmail.com>
2024-03-11 12:03:30 +0000

    Atualiza status com informações do banco de dados

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -7,9 +7,35 @@ import database from "infra/database.js"
 //npm run dev
 
 async function status(request, response) {
-    const result = await database.query("select 1 + 1 as sum;")
-    console.log(result.rows);
-    response.status(200).json("Testé são um teste")
+    //Data
+    const updatedAt = new Date().toISOString();
+
+    const dbVersion = await database.query("SHOW server_version;");
+    const dbMaxCon = await database.query("SHOW max_connections;");
+    const dbUsedCon = await database.query("SELECT count(*) AS conexoes_ativas FROM pg_stat_activity;");
+
+    console.log(dbMaxCon.rows[0].max_connections);
+
+
+    //Versão do postgress
+    const pgVersion = dbVersion.rows[0].server_version;
+
+    //Conexões máximas
+    const maxConections = parseInt(dbMaxCon.rows[0].max_connections);
+
+    //Conexões abertas
+    const openedConnections = parseInt(dbUsedCon.rows[0].conexoes_ativas);
+
+    response.status(200).json({
+        updated_at: updatedAt,
+        dependecies: {
+            database: {
+                version: pgVersion,
+                max_conections: maxConections,
+                opened_connections: openedConnections,
+            }
+        }
+    });
 }
 
 export default status;

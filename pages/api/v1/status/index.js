f2f60bff63e720561e39500af7a33085390743bb
Bruno Henrique <bruno_henriquemo@hotmail.com>
2025-04-21 02:28:07 +0000

    feat: create and use `InternalServerError` in `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,5 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/errors.js";
 
 // para validar os testes é necessário o servidor web e
 // o serviço do banco de dados estarem rodando
@@ -7,36 +8,47 @@ import database from "infra/database.js";
 //npm run dev
 
 async function status(request, response) {
-    //Data
-    const updatedAt = new Date().toISOString();
-    const dbName = process.env.POSTGRES_DB;
+    try {
+        //Data
+        const updatedAt = new Date().toISOString();
+        const dbName = process.env.POSTGRES_DB;
 
-    const dbVersion = await database.query("SHOW server_version;");
-    const dbMaxCon = await database.query("SHOW max_connections;");
-    const dbUsedCon = await database.query({
-        text: "SELECT count(*) AS conexoes_ativas FROM pg_stat_activity WHERE datname = $1;",
-        values: [dbName],
-    });
+        const dbVersion = await database.query("SHOW server_version;");
+        const dbMaxCon = await database.query("SHOW max_connections;");
+        const dbUsedCon = await database.query({
+            text: "SELECT count(*) AS conexoes_ativas FROM pg_stat_activity WHERE datname = $1;",
+            values: [dbName],
+        });
 
-    //Versão do postgress
-    const pgVersion = dbVersion.rows[0].server_version;
+        //Versão do postgress
+        const pgVersion = dbVersion.rows[0].server_version;
 
-    //Conexões máximas
-    const maxConections = parseInt(dbMaxCon.rows[0].max_connections);
+        //Conexões máximas
+        const maxConections = parseInt(dbMaxCon.rows[0].max_connections);
 
-    //Conexões abertas
-    const openedConnections = parseInt(dbUsedCon.rows[0].conexoes_ativas);
+        //Conexões abertas
+        const openedConnections = parseInt(dbUsedCon.rows[0].conexoes_ativas);
 
-    response.status(200).json({
-        updated_at: updatedAt,
-        dependecies: {
-            database: {
-                version: pgVersion,
-                max_connections: maxConections,
-                opened_connections: openedConnections,
+        response.status(200).json({
+            updated_at: updatedAt,
+            dependecies: {
+                database: {
+                    version: pgVersion,
+                    max_connections: maxConections,
+                    opened_connections: openedConnections,
+                },
             },
-        },
-    });
+        });
+    } catch (error) {
+        const publicErrorObject = new InternalServerError({
+            cause: error,
+        });
+
+        console.error("\n Erro dentro do catch do controller");
+        console.error(publicErrorObject);
+
+        response.status(500).json(publicErrorObject);
+    }
 }
 
 export default status;

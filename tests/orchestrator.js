66897367eb4841b337c6d8b9d4c878f1725f0e8a
Bruno Henrique <bruno_henriquemo@hotmail.com>
2026-01-07 12:38:02 +0000

    feat: add `email.js` infra module

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -6,8 +6,11 @@ import { migrator } from "models/migrator";
 import user from "models/user";
 import session from "models/session";
 
+const emailHttpUrl = `http://${process.env.EMAIL_HTTP_HOST}:${process.env.EMAIL_HTTP_PORT}`;
+
 async function waitForAllServices() {
   await waitForWebServer();
+  await waitForEmailServer();
 
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
@@ -18,6 +21,20 @@ async function waitForAllServices() {
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
       // const responseBody = await response.json();
+      if (response.status !== 200) {
+        throw Error();
+      }
+    }
+  }
+
+  async function waitForEmailServer() {
+    return retry(fetchEmailPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
+
+    async function fetchEmailPage() {
+      const response = await fetch(emailHttpUrl);
       if (response.status !== 200) {
         throw Error();
       }
@@ -46,12 +63,32 @@ async function createSession(userId) {
   return await session.create(userId);
 }
 
+async function deleteAllEmails() {
+  await fetch(`${emailHttpUrl}/messages`, { method: "DELETE" });
+}
+
+async function getLastEmail() {
+  const response = await fetch(`${emailHttpUrl}/messages`);
+  const emails = await response.json();
+  const lastEmailItem = emails.pop();
+
+  const emailTextRepsponse = await fetch(
+    `${emailHttpUrl}/messages/${lastEmailItem.id}.plain`,
+  );
+  const emailTextBody = await emailTextRepsponse.text();
+
+  lastEmailItem.text = emailTextBody;
+  return lastEmailItem;
+}
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
   runPendingMigrations,
   createUser,
   createSession,
+  deleteAllEmails,
+  getLastEmail,
 };
 
 export default orchestrator;

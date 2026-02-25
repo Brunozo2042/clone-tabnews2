45deaa177507472ac93fa0004c8cb4b2eccda200
Bruno Henrique <bruno_henriquemo@hotmail.com>
2026-02-25 00:05:47 +0000

    feat: send activation email after `user` registration

---
+++ models/activation.js
@@ -0,0 +1,69 @@
+import email from "infra/email.js";
+import database from "infra/database";
+import webserver from "infra/webserver.js";
+
+const EXPIRATION_IN_MILISECONDS = 60 * 15 * 1000; // 15 minutos
+
+async function findOneByUserId(userId) {
+  const newToken = await runSelectQuery(userId);
+  return newToken;
+
+  async function runSelectQuery(userId) {
+    const result = await database.query({
+      text: `
+        SELECT
+          *
+        FROM
+          user_activation_tokens
+        WHERE
+          user_id = $1
+        LIMIT 1
+      ;`,
+      values: [userId],
+    });
+    return result.rows[0];
+  }
+}
+
+async function create(userId) {
+  const expiresAt = new Date(Date.now() + EXPIRATION_IN_MILISECONDS);
+  const newToken = await runInsertQuery(userId, expiresAt);
+  return newToken;
+
+  async function runInsertQuery(userId, expiresAt) {
+    const result = await database.query({
+      text: `
+      INSERT INTO 
+        user_activation_tokens (user_id, expires_at)
+      VALUES 
+        ($1, $2)
+      RETURNING
+        *
+    ;`,
+      values: [userId, expiresAt],
+    });
+    return result.rows[0];
+  }
+}
+
+async function sendEmailToUser(user, activationToken) {
+  await email.send({
+    from: "teste@teste.com.br",
+    to: user.email,
+    subject: "Ative seu cadastro no FinTab!",
+    text: `${user.username}, clique no link abaixo para ativar seu cadastro: 
+
+${webserver.origin}/cadastro/ativar/${activationToken.id}
+
+Atenciosamente, equipe FinTab
+    `,
+  });
+}
+
+const activation = {
+  findOneByUserId,
+  create,
+  sendEmailToUser,
+};
+
+export default activation;
--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -1,6 +1,7 @@
 import { createRouter } from "next-connect";
 import controller from "infra/controller";
 import user from "models/user";
+import activation from "models/activation";
 
 const router = createRouter();
 
@@ -11,5 +12,11 @@ export default router.handler(controller.errorHandlers);
 async function postHandler(request, response) {
   const userInputValues = request.body;
   const newUser = await user.create(userInputValues);
+
+  // 1. Criar token de ativação
+  const activationToken = await activation.create(newUser.id);
+  // 2. Enviar token por email
+  await activation.sendEmailToUser(newUser, activationToken);
+
   return response.status(201).json(newUser);
 }

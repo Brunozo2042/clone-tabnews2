dd66a4ad2203d364b94ae8892c3e31cd2afc82b2
Bruno Henrique <bruno_henriquemo@hotmail.com>
2026-06-20 13:04:39 +0000

    feat: require `create:user` to access `/api/v1/users`

--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -5,7 +5,8 @@ import activation from "models/activation";
 
 const router = createRouter();
 
-router.post(postHandler);
+router.use(controller.injectAnounymousOrUser);
+router.post(controller.canRequest("create:user"), postHandler);
 
 export default router.handler(controller.errorHandlers);
 

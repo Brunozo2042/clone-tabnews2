5bb44e71a58e417ece7c7d448c1ca88c6d990cad
Bruno Henrique <bruno_henriquemo@hotmail.com>
2026-06-20 18:29:51 +0000

    feat: consider `resource` in `authorization` model

--- pages/api/v1/users/[username]/index.js
+++ pages/api/v1/users/[username]/index.js
@@ -1,12 +1,14 @@
 import { createRouter } from "next-connect";
 import controller from "infra/controller";
 import user from "models/user";
+import authorization from "models/authorization.js";
+import { ForbiddenError } from "infra/errors.js";
 
 const router = createRouter();
 
 router.use(controller.injectAnounymousOrUser);
 router.get(getHandler);
-router.patch(controller.canRequest("update:user"),patchHandler);
+router.patch(controller.canRequest("update:user"), patchHandler);
 
 export default router.handler(controller.errorHandlers);
 
@@ -19,6 +21,20 @@ async function getHandler(request, response) {
 async function patchHandler(request, response) {
   const username = request.query.username;
   const userInputValues = request.body;
+
+  // user, feature, resource
+  const userTryingToPatch = request.context.user;
+  const targetUser = await user.findOneByUsername(username);
+
+  if (!authorization.can(userTryingToPatch, "update:user", targetUser)) {
+    throw new ForbiddenError({
+      message: "Você não possui permissão para atualizar outro usuário.",
+      action:
+        "Verifique se você possui a feature necessária para atualizar outro usuário",
+      statusCode: 403,
+    });
+  }
+
   const updatesUser = await user.update(username, userInputValues);
   return response.status(200).json(updatesUser);
 }
